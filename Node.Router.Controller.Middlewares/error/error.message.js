08e68a65c0e1b2b45396312e920201e64f924500
PrystaikoVolodymyr <volodymyr.p445@gmail.com>
2021-07-22 11:49:37 +0300

    Router.Controller.Middlewares

--- Node.Router.Controller.Middlewares/error/error.message.js
+++ Node.Router.Controller.Middlewares/error/error.message.js
@@ -1,10 +1,10 @@
 module.exports = {
     BAD_USER: {
-        en: "User is already created",
-        ua: "Користувач вже створений"
+        en: 'User is already created',
+        ua: 'Користувач вже створений'
     },
     BAD_ID: {
-        en: "Bad Id",
-        ua: "Погане Id"
+        en: 'Bad Id',
+        ua: 'Погане Id'
     }
-}
+};
